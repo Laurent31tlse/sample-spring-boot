fd0751a2be1c0d65794dd2c60e7c5d3320cf0b62
Laurent31tlse <marcotesa2001@yahoo.fr>
2017-04-28 15:03:50 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -14,24 +14,5 @@ public class DemoApplicationTests {
 	public void contextLoads() {
 		
 	}
-	@Test
-	public final void testMultiply() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testDivide() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testAdd() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testSubstract() {
-		fail("Not yet implemented"); // TODO
-	}
-
+	
 }
