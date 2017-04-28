4ef66a005c93e037a80d3174b4c208133cebcffb
Laurent31tlse <marcotesa2001@yahoo.fr>
2017-04-28 15:06:14 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Ignore;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
