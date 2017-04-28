8e329772948d4450156322daef7886249aa81b2c
Laurent31tlse <marcotesa2001@yahoo.fr>
2017-04-28 15:01:17 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Ignore;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,11 +14,11 @@ public class DemoApplicationTests {
 	public void contextLoads() {
 		
 	}
-	/*@ignore
+	@ignore
 	@Test
     	public final void testMultiply() {
         fail("Not yet implemented"); // TODO
 
-    }*/
+    }
 
 }
